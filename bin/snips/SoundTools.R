fb6aec977642a0d0790f83c54451d614e9732c32
EwanP <ewan@ewanp.com>
2018-10-27 13:25:40 +0000

    EAP Push Tidy classifier_results & Snips

--- bin/snips/SoundTools.R
+++ bin/snips/SoundTools.R
@@ -10,3 +10,9 @@ date_time <- gsub("_", "", substr(file_name,5,19))
 DateTime <- as.POSIXct(date_time, format="%Y%m%d%H%M%S")
 #Putting it all into a single line to give all data and times in a list
 DateTime <- as.POSIXct(gsub("_", "", substr(ECC_Records$filename,5,19)), format="%Y%m%d%H%M%S")
+#Now create a new DataFrame with the dates/times as a new column
+TestFrame <- data.frame(obs_datetime = as.POSIXct(gsub("_", "", substr(ECC_Records$filename,5,19)), format="%Y%m%d%H%M%S"), ECC_Records)
+#Or to ammend the existing dataframe
+ECC_Records <- data.frame(obs_datetime = as.POSIXct(gsub("_", "", substr(ECC_Records$filename,5,19)), format="%Y%m%d%H%M%S"), ECC_Records)
+# Finally write the new dataframe out as a csv
+write.csv(ECC_Records, "./R-Test/tidy/ECC/ECC_txt_classifier_results.csv", row.names = FALSE)
